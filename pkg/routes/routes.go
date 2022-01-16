3f70a75203438eea2fbc1052b4f759ded187f734
Cesar Perez <cesaraugp01@gmail.com>
2022-01-16 10:45:50 -0400

    ADD Remov Organization Route close #3

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -10,11 +10,11 @@ func PublicRoutes(a *fiber.App) {
 	route := a.Group("/api/v1")
 	route.Post("/organization", controllers.PostCreateOrganization)
 	route.Put("/organization", controllers.PutEditOrganization)
+	route.Delete("/organization", controllers.DeleteRemoveOrganization)
 	route.Post("/test", controllers.TestToken)
 	/*
-		route.Delete("/organization/:id", controllers.PostCreateOrganization)
 		route.Get("/organization/:id", controllers.PostCreateOrganization)
-		 */
+	*/
 	//route.Get("/", controllers.GetRoutes) //Vamos a usar esta ruta en raiz para que devuelva todas las rutas del microservicio, su metodo y su body
 	//route.Post("/sign-in", controllers.PostSignIn)
 }
